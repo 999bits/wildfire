2010ba621f892562f1c8c1adb1e6817623b06c94
rickuser9600 <rsimon9600@gmail.com>
2023-06-30 03:33:30 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -34,57 +34,57 @@ describe("Wildfire", function () {
     console.log("wildfire address",wildfire.address)
   });
 
-  // it("should create a sell order", async function () {
-  //   await tradeToken.mint(maker.address, epochId, sellAmount, {gasLimit : 50000});
+  it("should create a sell order", async function () {
+    await tradeToken.mint(maker.address, epochId, sellAmount, {gasLimit : 50000});
 
-  //   await expect(wildfire.connect(maker).createSellOrder(tradeToken.address, sellPrice, sellAmount, epochId)).to.emit(wildfire, "OrderCreated").withArgs(1, maker.address, 0);
+    await expect(wildfire.connect(maker).createSellOrder(tradeToken.address, sellPrice, sellAmount, epochId)).to.emit(wildfire, "OrderCreated").withArgs(1, maker.address, 0);
 
-  //   const sellOrder = await wildfire.sellOrders(0);
-  //   expect(sellOrder.orderId).to.equal(1);
-  //   expect(sellOrder.token).to.equal(tradeToken.address);
-  //   expect(sellOrder.price).to.equal(sellPrice);
-  //   expect(sellOrder.amount).to.equal(sellAmount);
-  //   expect(sellOrder.creator).to.equal(maker.address);
-  //   expect(sellOrder.orderType).to.equal(0);
-  //   expect(sellOrder.status).to.equal(false);
-  // });
+    const sellOrder = await wildfire.sellOrders(0);
+    expect(sellOrder.orderId).to.equal(1);
+    expect(sellOrder.token).to.equal(tradeToken.address);
+    expect(sellOrder.price).to.equal(sellPrice);
+    expect(sellOrder.amount).to.equal(sellAmount);
+    expect(sellOrder.creator).to.equal(maker.address);
+    expect(sellOrder.orderType).to.equal(0);
+    expect(sellOrder.status).to.equal(false);
+  });
 
-  // it("should create a buy order", async function () {
+  it("should create a buy order", async function () {
 
-  //   await payToken.connect(taker).deposit({value: ethers.utils.parseEther("5")});
-  //   await expect(wildfire.connect(taker).createBuyOrder(tradeToken.address, buyPrice, buyAmount, {gasLimit : 1000000})).to.emit(wildfire, "OrderCreated").withArgs(1, taker.address, 1);
+    await payToken.connect(taker).deposit({value: ethers.utils.parseEther("5")});
+    await expect(wildfire.connect(taker).createBuyOrder(tradeToken.address, buyPrice, buyAmount, {gasLimit : 1000000})).to.emit(wildfire, "OrderCreated").withArgs(1, taker.address, 1);
 
-  //   const buyOrder = await wildfire.buyOrders(0);
-  //   expect(buyOrder.orderId).to.equal(1);
-  //   expect(buyOrder.token).to.equal(tradeToken.address);
-  //   expect(buyOrder.price).to.equal(buyPrice);
-  //   expect(buyOrder.amount).to.equal(buyAmount);
-  //   expect(buyOrder.creator).to.equal(taker.address);
-  //   expect(buyOrder.orderType).to.equal(1);
-  //   expect(buyOrder.status).to.equal(false);
-  // });
+    const buyOrder = await wildfire.buyOrders(0);
+    expect(buyOrder.orderId).to.equal(1);
+    expect(buyOrder.token).to.equal(tradeToken.address);
+    expect(buyOrder.price).to.equal(buyPrice);
+    expect(buyOrder.amount).to.equal(buyAmount);
+    expect(buyOrder.creator).to.equal(taker.address);
+    expect(buyOrder.orderType).to.equal(1);
+    expect(buyOrder.status).to.equal(false);
+  });
 
-  // it("should cancel a sell order", async function () {
+  it("should cancel a sell order", async function () {
 
-  //   await tradeToken.mint(maker.address, epochId, sellAmount, {gasLimit : 50000});
+    await tradeToken.mint(maker.address, epochId, sellAmount, {gasLimit : 50000});
 
-  //   await wildfire.connect(maker).createSellOrder(tradeToken.address, sellPrice, sellAmount, epochId, {gasLimit : 1000000});
-  //   await expect(wildfire.connect(maker).cancelOrder(1, 0)).to.emit(wildfire, "OrderCanceled").withArgs(1, maker.address, 0);
+    await wildfire.connect(maker).createSellOrder(tradeToken.address, sellPrice, sellAmount, epochId, {gasLimit : 1000000});
+    await expect(wildfire.connect(maker).cancelOrder(1, 0)).to.emit(wildfire, "OrderCanceled").withArgs(1, maker.address, 0);
 
-  //   const sellOrder = await wildfire.sellOrders(0);
-  //   expect(sellOrder.status).to.equal(true);
-  // });
+    const sellOrder = await wildfire.sellOrders(0);
+    expect(sellOrder.status).to.equal(true);
+  });
 
-  // it("should cancel a buy order", async function () {
+  it("should cancel a buy order", async function () {
 
-  //   await payToken.connect(taker).deposit({value: ethers.utils.parseEther("5")});
+    await payToken.connect(taker).deposit({value: ethers.utils.parseEther("5")});
 
-  //   await wildfire.connect(taker).createBuyOrder(tradeToken.address, buyPrice, buyAmount);
-  //   await expect(wildfire.connect(taker).cancelOrder(1, 1)).to.emit(wildfire, "OrderCanceled").withArgs(1, taker.address, 1);
+    await wildfire.connect(taker).createBuyOrder(tradeToken.address, buyPrice, buyAmount);
+    await expect(wildfire.connect(taker).cancelOrder(1, 1)).to.emit(wildfire, "OrderCanceled").withArgs(1, taker.address, 1);
 
-  //   const buyOrder = await wildfire.buyOrders(0);
-  //   expect(buyOrder.status).to.equal(true);
-  // });
+    const buyOrder = await wildfire.buyOrders(0);
+    expect(buyOrder.status).to.equal(true);
+  });
 
   it("should fulfill a sell order", async function () {
     const sellToken = tradeToken.address;
