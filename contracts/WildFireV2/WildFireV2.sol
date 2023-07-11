0fda760965b97c30f87c9b166cf1ee4d4cacc3bb
rickuser9600 <rsimon9600@gmail.com>
2023-07-11 02:25:08 -0700

    writing updated orderbook contract
    Done of structure and mapping variables   and Implemented some helper functions

---
+++ contracts/WildFireV2/WildFireV2.sol
@@ -0,0 +1,370 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IWBNB} from "../IWBNB.sol";
+import "hardhat/console.sol";
+
+contract WildfireV2 is ReentrancyGuard, ERC1155Holder {
+    IERC1155 public tradeToken; // ERC1155 address
+    IWBNB public payToken; // BNB address
+
+    // Order Info for managing only maker and amount
+    struct Order {
+        address maker;
+        uint256 orderAmount;
+    }
+    // Order Grid for managing by order
+    struct OrderGrid {
+        bytes32 nextOrderGridId; // next order grid id in OrderGridList/grids
+        Order order;
+    }
+    // Order Grid list for managing by order
+    struct OrderGridList {
+        uint256 length;
+        bytes32 headId;
+        bytes32 tailId;
+        mapping(bytes32 => OrderGrid) grids; // bytes32 : unique id for OrderGrid
+    }
+    // Price Grid for managing by price
+    struct PriceGrid {
+        uint256 price;
+        uint256 priceAmount;
+    }
+    // Order-Price Grid for managing combination with order and price
+    struct OrderPriceGrid {
+        bytes32 orderId;
+        uint256 orderPrice;
+        uint256 orderAmount;
+    }
+    struct OrderPriceGridList {
+        mapping(address => OrderPriceGrid[]) orders; // address : orderMaker address
+        mapping(bytes32 => uint256) orderIndexes; // bytes32 : orderId
+    }
+
+    enum SellOrBuy {
+        Sell,
+        Buy
+    }
+    PriceGrid[] public sellPrices; // sell price list
+    PriceGrid[] public buyPrices; // buy price list
+    OrderPriceGridList public sellOrders; // sell order list
+    OrderPriceGridList public buyOrders; // buy order list
+
+    // depositListPerUser[userAddress][tokenAddress] => depositAmount
+    mapping(address => mapping(address => uint256)) public depositListPerUser; // address(1st parameter) : user address, address(2nd parameter) : token address
+
+    // orderBook[tokenAddress][price] => orders[maker, orderAmount]
+    mapping(address => mapping(uint256 => OrderGridList)) public orderBook; // address(1st parameter) : token address, uint256(2nd parameter) : price
+
+    event OrderCreated();
+    event OrderFilled();
+    event OrderPartiallyFilled();
+    event OrderCancelled();
+
+    constructor(address _tradeToken, address _payToken) {
+        tradeToken = IERC1155(_tradeToken); // ERC1155 token address
+        payToken = IWBNB(_payToken); // USDC address
+    }
+
+    /**
+     * Create an order to sell the token.
+     * should init price grid before call this at first
+     * @param _price desired price
+     * @param _sellAmount sell token amount
+     * @param _priceIndex index of price list
+     *
+     */
+    function createSellOrder(
+        uint256 _price,
+        uint256 _sellAmount,
+        uint256 _priceIndex
+    ) external returns (bool) {
+        require(_price > 0, "Price should be greater than zero");
+        require(_sellAmount > 0, "SellAmount should be greater than zero");
+
+        (bool isExistingPrice, uint256 index) = getIndexOfPrice(
+            _price,
+            SellOrBuy.Sell
+        );
+        if (!isExistingPrice) initPriceGrid(_price);
+
+        require(
+            sellPrices[_priceIndex].price == _price &&
+                buyPrices[_priceIndex].price == _price,
+            "Price does not match the index"
+        );
+        bytes32 orderId;
+
+        // new sell order
+        if (orderBook[tradeToken][_price].length == 0) {
+            orderId = initOrderGridList(
+                orderBook[tradeToken][_price],
+                msg.sender,
+                _sellAmount
+            );
+        } else {
+            orderId = addOrderGridList(
+                orderBook[tradeToken][_price],
+                msg.sender,
+                _sellAmount
+            );
+        }
+        addOrderPriceGridList(
+            sellOrders,
+            msg.sender,
+            orderId,
+            _price,
+            _sellAmount
+        );
+        addPriceAmount(sellPrices, _priceIndex, _sellAmount);
+    }
+
+    /**
+     * Create an order to buy token.
+     *
+     * @param _price desired price
+     * @param _buyAmount sell token amount
+     * @param _priceIndex index of price list
+     *
+     */
+    function createBuyOrder(
+        uint256 _price,
+        uint256 _buyAmount,
+        uint256 _priceIndex
+    ) external returns (bool) {}
+
+    /**
+     * Cancel the sell order.
+     *
+     * @param _price  price to cancel order
+     * @param _orderId order id to cancel order
+     * @param _priceIndex index of price list
+     *
+     */
+    function cancelSellOrder(
+        uint256 _price,
+        bytes32 _orderId,
+        uint256 _priceIndex
+    ) external returns (bool) {}
+
+    /**
+     * Cancel the buy order.
+     *
+     * @param _price  price to cancel order
+     * @param _orderId order id to cancel order
+     * @param _priceIndex index of price list
+     *
+     */
+    function cancelBuyOrder(
+        uint256 _price,
+        bytes32 _orderId,
+        uint256 _priceIndex
+    ) external returns (bool) {}
+
+    /**
+     * Initiate price grid with specific price
+     *
+     * @param _token  deposit token address
+     * @param _amount deposit token amount
+     * @param _epochId epochId for ERC1155 token
+     *
+     */
+    function deposit(
+        address _token,
+        uint256 _amount,
+        uint256 _epochId
+    ) public returns (bool) {
+        require(
+            _token == tradeToken || _token == payToken,
+            "Deposited token is not supported token type"
+        );
+        if (_token == tradeToken) {
+            tradeToken.safeTransferFrom(
+                msg.sender,
+                address(this),
+                _epochId,
+                _amount,
+                ""
+            );
+        } else {
+            payToken.transferFrom(msg.sender, address(this), _amount);
+        }
+    }
+
+    // PriceGrid Helper functions
+    /****************************************************************************************/
+    /**
+     * Initiate PriceGrid with specific price
+     *
+     * @param _price speicific price
+     *
+     */
+    function initPriceGrid(uint256 _price) external {
+        require(
+            orderBook[tradeToken][_price].tailId == "" &&
+                orderBook[payToken][_price].tailId == "",
+            "Price already exist in orderbook"
+        );
+        sellOB.push(PriceGrid(_price, 0));
+        buyOB.push(PriceGrid(_price, 0));
+    }
+
+    /**
+     * Add priceAmount of PriceGrid
+     *
+     * @param _pGrid PriceGrid array
+     * @param _index _index of price array
+     * @param _changeAmount add amount
+     *
+     */
+    function addPriceAmount(
+        PriceGrid[] storage _pGrid,
+        uint256 _index,
+        uint256 _changeAmount
+    ) private returns (bool) {
+        _pGrid[_index].priceAmount += _changeAmount;
+        return true;
+    }
+
+    /**
+     * Subtract priceAmount of PriceGrid
+     *
+     * @param _pGrid PriceGrid array
+     * @param _index _index of price array
+     * @param _changeAmount add amount
+     *
+     */
+    function subPriceAmount(
+        PriceGrid[] storage _pGrid,
+        uint256 _index,
+        uint256 _changeAmount
+    ) private returns (bool) {
+        _pGrid[_index].priceAmount -= _changeAmount;
+        return true;
+    }
+
+    /**
+     * Get price grid index with specific price
+     *
+     * @param _price speicific price
+     * @param _key sell or buy
+     *
+     */
+    function getIndexOfPrice(
+        uint256 _price,
+        SellOrBuy _key
+    ) public view returns (bool, uint256) {
+        if (_key == SellOrBuy.Sell) {
+            for (uint256 i = 0; i < sellPrices.length; i++) {
+                if (sellPrices[i].price == _price) {
+                    return (true, i);
+                }
+            }
+        } else {
+            for (uint256 i = 0; i < buyPrices.length; i++) {
+                if (buyPrices[i].price == _price) {
+                    return (true, i);
+                }
+            }
+        }
+        return (false, 0);
+    }
+
+    // OrderGridList Helper functions
+    /****************************************************************************************/
+    /**
+     * Initiate OrderGridList with a new order
+     *
+     * @param _orderGridList OrderGridList
+     * @param _maker maker of a new order
+     * @param _amount amount of a new order
+     *
+     */
+    function initOrderGridList(
+        OrderGridList storage _orderGridList,
+        address _maker,
+        uint256 _amount
+    ) private returns (bytes32) {
+        Order memory order = Order(_maker, _amount);
+        OrderGrid memory orderGrid = OrderGrid(0, order);
+
+        bytes32 id = keccak256(
+            abi.encodePacked(
+                _maker,
+                _amount,
+                _orderGridList.length,
+                block.timestamp
+            )
+        );
+        _orderGridList.length = 1;
+        _orderGridList.headId = id;
+        _orderGridList.tailId = id;
+        _orderGridList.grids[id] = orderGrid;
+
+        return id;
+    }
+
+    /**
+     * Add a new order to OrderGridList
+     *
+     * @param _orderGridList OrderGridList
+     * @param _maker maker of a new order
+     * @param _amount amount of a new order
+     *
+     */
+    function addOrderGridList(
+        OrderGridList storage _orderGridList,
+        address _maker,
+        uint256 _amount
+    ) private returns (bytes32) {
+        Order memory order = Order(_maker, _amount);
+        OrderGrid memory orderGrid = OrderGrid(0, order);
+
+        bytes32 id = keccak256(
+            abi.encodePacked(
+                _maker,
+                _amount,
+                _orderGridList.length,
+                block.timestamp
+            )
+        );
+        _orderGridList.length += 1;
+        _orderGridList.tailId = id;
+        _orderGridList.grids[id] = orderGrid;
+        _orderGridList.grids[_orderGridList.tailId].nextOrderGridId = id;
+        return id;
+    }
+
+    // OrderPriceGrid Helper functions
+    /****************************************************************************************/
+    /**
+     * Add new order to OrderPriceGridList
+     *
+     * @param _list OrderPriceGridList
+     * @param _maker maker of a new order
+     * @param _orderId orderid of a new order
+     * @param _price price of a new order
+     * @param _amount amount of a new order
+     *
+     */
+    function addOrderPriceGridList(
+        OrderPriceGridList storage _list,
+        address _maker,
+        bytes32 _orderId,
+        uint32 _price,
+        uint256 _amount
+    ) internal returns (bool) {
+        if (_list._indexes[_orderId] == 0) {
+            _list._orders[_maker].push(
+                OrderPriceGrid(_orderId, _price, _amount)
+            );
+            _list._indexes[_orderId] = _list._orders[_maker].length;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
